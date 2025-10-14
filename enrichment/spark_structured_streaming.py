8792511b30b44c7e65779cddaa3fceba93b942cf
onitnas <santinoisgr7@gmail.com>
2025-10-14 22:47:42 +0200

    feat: add Spark master/worker services and Kafka streaming integration

---
+++ enrichment/spark_structured_streaming.py
@@ -0,0 +1,69 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, from_json
+
+class SparkStructuredStreaming:
+    def __init__(self, app_name="SparkStructuredStreamingApp"):
+        self.schema = """
+            match_id STRING,
+            timestamp STRING,
+            set INT,
+            game INT,
+            point INT,
+            server STRING,
+            receiver STRING,
+            server_score INT,
+            receiver_score INT,
+            event STRING,
+            winner STRING,
+            serve_speed_kmh DOUBLE,
+            court_surface STRING,
+            tournament STRING,
+            round STRING,
+            location STRING
+        """
+        self.spark = SparkSession.builder.appName(app_name) \
+        .master("spark://spark-master:7077") \
+        .getOrCreate()
+        self.spark.sparkContext.setLogLevel("ERROR")
+        
+    def read_stream(self, kafka_bootstrap_servers, topic):
+        df = self.spark.readStream \
+            .format("kafka") \
+            .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
+            .option("subscribe", topic) \
+            .option("startingOffsets", "latest") \
+            .load()
+            
+        return df.selectExpr("CAST(value AS STRING)") \
+            .select(from_json(col("value"), self.schema).alias("data")) \
+            .select("data.*")
+    
+    def write_historycal_db(self, df, output_path, checkpoint_location):
+        df \
+            .writeStream \
+            .outputMode("append") \
+            .format("parquet") \
+            .option("path", output_path) \
+            .option("checkpointLocation", checkpoint_location) \
+            .start()
+    
+    
+        
+    def write_kafka(self, df, kafka_bootstrap_servers, topic):
+        df.selectExpr("to_json(struct(*)) AS value") \
+            .writeStream \
+            .format("kafka") \
+            .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
+            .option("topic", topic) \
+            .start()
+    
+    
+if __name__ == "__main__":
+    kafka_bootstrap_servers = "kafkaServer:9092"
+    topic = "new_point"
+    output_path = ""
+    checkpoint_location = ""
+    
+    streaming_app = SparkStructuredStreaming()
+    df = streaming_app.read_stream(kafka_bootstrap_servers, topic)
+    streaming_app.write_stream(df, output_path, checkpoint_location)
