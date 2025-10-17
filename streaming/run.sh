b62b7f09be3ec723d8348c3daf04352b77be5a79
onitnas <santinoisgr7@gmail.com>
2025-10-17 12:35:34 +0200

    feat: enable Elasticsearch security, create keystore and truststore for Kafka Connect, and draft Kibana dashboard

--- streaming/run.sh
+++ streaming/run.sh
@@ -3,9 +3,33 @@ until curl -location http://kafka-connect:8083/; do
   sleep 2
 done
 
+echo "Create truststore and keystore..."
+openssl pkcs12 -export -out bundle.p12 \
+  -in /usr/share/elasticsearch/config/certs/elasticsearch/elasticsearch.crt \
+  -inkey /usr/share/elasticsearch/config/certs/elasticsearch/elasticsearch.key \
+  -name elasticsearch \
+  -passout pass:rootlibero
+
+keytool -import \
+  -keystore /usr/share/elasticsearch/config/certs/truststore.jks \
+  -file /usr/share/elasticsearch/config/certs/ca/ca.crt \
+  -alias cacert \
+  -storepass rootlibero \
+  -noprompt
+
+keytool -destkeystore /usr/share/elasticsearch/config/certs/keystore.jks \
+  -importkeystore \
+  -srckeystore bundle.p12 \
+  -srcstoretype PKCS12 \
+  -srcstorepass rootlibero \
+  -deststorepass rootlibero \
+  -storepass rootlibero \
+  -noprompt
+
 echo "Ensure index template exists..."
 
-curl -sS -X PUT 'http://elasticsearch:9200/_index_template/tmf_new_point_template' \
+curl -sS -u elastic:rootlibero --cacert /usr/share/elasticsearch/config/certs/ca/ca.crt \
+  -X PUT 'https://elasticsearch:9200/_index_template/tmf_new_point_template' \
   -H 'Content-Type: application/json' \
   --data-binary '{
     "index_patterns": ["new_point*"],
